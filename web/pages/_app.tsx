0319942461a3cd80b0a1ea995f2e6960a014a830
karimLa <kira00luffy@gmail.com>
2021-03-29 20:14:34 +0100

    added header

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,29 +1,13 @@
 import { AppProps } from 'next/app';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-	colors: {
-		primary: 'red',
-	},
-};
+import Page from '@/components/Page';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<GlobalStyle />
-			<ThemeProvider theme={theme}>
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
 
 export default MyApp;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,16 +1,14 @@
 import Head from 'next/head';
 
-import SickButton from '@/components/styles/SickButton';
-
 export default function Home() {
-	return (
-		<main>
-			<Head>
-				<title>OrinuFit</title>
-				<link rel='icon' href='/static/favicon.png' />
-			</Head>
+  return (
+    <main>
+      <Head>
+        <title>OrinuFit</title>
+        <link rel='icon' href='/static/favicon.png' />
+      </Head>
 
-			<SickButton>Click me</SickButton>
-		</main>
-	);
+      <h3>Home Page</h3>
+    </main>
+  );
 }
