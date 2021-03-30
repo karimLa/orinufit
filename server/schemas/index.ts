8c0ce9e3533591ee32a0a47fcea5f86a7c9b489e
karimLa <kira00luffy@gmail.com>
2021-03-30 12:30:38 +0100

    added Product data type

--- server/schemas/index.ts
+++ server/schemas/index.ts
@@ -1,9 +1,11 @@
 import { createSchema } from '@keystone-next/keystone/schema';
 
 import { User } from './User';
+import { Product } from './Product';
 
 const lists = createSchema({
   User,
+  Product,
 });
 
 export default lists
