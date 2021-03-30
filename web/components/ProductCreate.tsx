7685ab9892dca0010c97e105d45525843428f936
karimLa <kira00luffy@gmail.com>
2021-03-30 22:05:04 +0100

    refetching products after product create

--- web/components/ProductCreate.tsx
+++ web/components/ProductCreate.tsx
@@ -5,6 +5,7 @@ import { useMutation } from '@apollo/client';
 import Form from './styles/Form';
 import useForm from '@/lib/useForm';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -42,6 +43,7 @@ function ProductCreate() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
